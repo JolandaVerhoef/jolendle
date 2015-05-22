419e9feb8d474995de136d040b0164682207f817
Jolanda Verhoef <jolanda.verhoef@beethree.nl>
2015-05-22 23:53:55 +0200

    Use a FragmentStatePagerAdapter instead of FragmentPagerAdapter, because it is recommended when you have lots of pages in the viewpager.

--- app/src/main/java/today/created/blendle/adapter/ItemPagerAdapter.java
+++ app/src/main/java/today/created/blendle/adapter/ItemPagerAdapter.java
@@ -2,7 +2,7 @@ package today.created.blendle.adapter;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v13.app.FragmentStatePagerAdapter;
 
 import java.util.List;
 
@@ -13,7 +13,7 @@ import today.created.blendle.view.ItemFragment;
  * Created by jolandaverhoef on 22/05/15.
  * An adapter that shows a Blendle item on each page.
  */
-public class ItemPagerAdapter extends FragmentPagerAdapter {
+public class ItemPagerAdapter extends FragmentStatePagerAdapter {
     private List<HalItemPopular> items;
 
     public ItemPagerAdapter(FragmentManager fm) {
