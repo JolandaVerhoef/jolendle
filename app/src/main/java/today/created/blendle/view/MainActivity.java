ae32d456d6fcbd18633a14c2756246242467c551
Jolanda Verhoef <jolanda.verhoef@beethree.nl>
2015-05-23 21:56:30 +0200

    On activity start check if daydream is enabled. If not, show dialog with link to settings.

--- app/src/main/java/today/created/blendle/view/MainActivity.java
+++ app/src/main/java/today/created/blendle/view/MainActivity.java
@@ -1,8 +1,18 @@
 package today.created.blendle.view;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
@@ -19,6 +29,7 @@ import today.created.blendle.hal.Link;
 
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
     private Link nextPage;
 
     /**
@@ -64,7 +75,33 @@ public class MainActivity extends Activity {
         });
         mViewPager.setAdapter(mItemPagerAdapter);
 
+        if(daydreamDisabled()) showDaydreamDialog();
         new TestTask().execute();
+    }
+
+    private void showDaydreamDialog() {
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        new DaydreamDialogFragment().show(ft, "dialog");
+    }
+
+    private boolean daydreamDisabled() {
+        Boolean daydreamEnabled = Settings.Secure.getInt(getContentResolver(),
+                "screensaver_enabled", 0) == 1;
+        Log.d(TAG, "Daydream enabled: " + daydreamEnabled);
+        if (!daydreamEnabled) return true;
+        String componentString = Settings.Secure.getString(getContentResolver(),
+                "screensaver_components");
+        return componentString == null || !componentString.contains(getApplicationContext().getPackageName());
     }
 
     private class TestTask extends AsyncTask<Void, Void, HalItemsPopular> {
@@ -115,5 +152,35 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class DaydreamDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.dialog_daydream_title)
+                    .setMessage(R.string.dialog_daydream_message)
+                    .setPositiveButton(R.string.dialog_daydream_enable, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent intent;
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                                intent = new Intent(Settings.ACTION_DREAM_SETTINGS);
+                            } else {
+                                intent = new Intent(Settings.ACTION_DISPLAY_SETTINGS);
+                            }
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                            dialog.cancel();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
     }
 }
